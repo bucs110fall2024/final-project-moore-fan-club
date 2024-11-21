d043de5329ef21af989591643ec9c4a1317b3fd3
Ryan Tsui <rtsui@binghamton.edu>
2024-11-20 20:25:09 -0500

    pygame open

--- finalprojectmaster/finalproject/main.py
+++ finalprojectmaster/finalproject/main.py
@@ -4,6 +4,24 @@ from src.main_menu import Main_menu
 
 def main():
     pygame.init()
+    pygame.display.set_mode()
+    def mainloop(self):
+        """
+        docstring
+        """
+        while(True): #this can also be a variable instead of just True
+      #1. Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+      #2. detect collisions and update models
+
+      #3. Redraw next frame
+
+      #4. Display next frame
+    pygame.display.flip()
     #Create an instance on your controller object
     #Call your mainloop
     
