5775835b5bb88ce8d22c2cc86935322b4c050375
Ryan Tsui <rtsui@binghamton.edu>
2024-11-25 17:23:23 -0500

    map

--- finalprojectmaster/finalproject/src/map.py
+++ finalprojectmaster/finalproject/src/map.py
@@ -1,5 +1,11 @@
 import pygame
+import googlemaps
 
 class Map:
+    gmaps = googlemaps.Client(key="YOUR_API_KEY")
+
+    geocode_result = gmaps.geocode('Binghamton University, New York')
+
+    print(geocode_result)
     def __init__(self):
         pass
